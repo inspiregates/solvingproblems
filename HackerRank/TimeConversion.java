813fab3583e0ebe92340cfdfae6dc82927145a21
Dhandapani Ponnurangam <inspiregates@gmail.com>
2018-09-17 18:35:40 -0700

    added link to the problem

--- HackerRank/TimeConversion.java
+++ HackerRank/TimeConversion.java
@@ -1,3 +1,10 @@
+/**************************************************************
+*
+* https://www.hackerrank.com/challenges/time-conversion/problem
+*
+***************************************************************/
+
+
 import java.io.*;
 import java.math.*;
 import java.text.*;
